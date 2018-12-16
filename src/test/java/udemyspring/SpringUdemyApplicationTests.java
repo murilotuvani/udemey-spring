9aa828668a52055d2bfab56d11572f0d8d116472
Murilo Tuvani <murilo.tuvani@gmail.com>
2018-12-16 00:20:38 -0200

    Trabalhando com varios profiles

--- src/test/java/udemyspring/SpringUdemyApplicationTests.java
+++ src/test/java/udemyspring/SpringUdemyApplicationTests.java
@@ -1,16 +1,29 @@
 package udemyspring;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class SpringUdemyApplicationTests {
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int quantidadeElementosPorPagina;
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testProfies() {
+		assertEquals(100, quantidadeElementosPorPagina);
 	}
 
 }
